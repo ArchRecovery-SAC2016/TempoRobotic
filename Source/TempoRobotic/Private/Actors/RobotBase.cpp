85952c41cebfa08a472c4656c17e9eb985617587
renatoedesio <renatoedesio@unb.br>
2026-01-02 15:02:06 -0300

    Creating the behavior tree

--- Source/TempoRobotic/Private/Actors/RobotBase.cpp
+++ Source/TempoRobotic/Private/Actors/RobotBase.cpp
@@ -1,8 +1,25 @@
 #include "Actors/RobotBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 ARobotBase ::ARobotBase () {
 	PrimaryActorTick.bCanEverTick = false;
 
+    // Garante que o robô tenha um AI Controller assim que nascer, 
+    // seja arrastado no editor ou spawnado via código.
+    AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+    
+    // Desacopla a rotação da câmera/controller direto
+    bUseControllerRotationYaw = false; 
+
+    // Permite que o CharacterMovement gire o mesh
+    GetCharacterMovement()->bOrientRotationToMovement = true; 
+    
+    // Define a velocidade de rotação (O quão rápido ele gira no próprio eixo)
+    // 180.0f = meio segundo para dar meia volta. Ajuste conforme o "motor" do seu robô.
+    GetCharacterMovement()->RotationRate = FRotator(0.0f, 180.0f, 0.0f);
+    
+    // Para robôs, é bom aumentar o atrito para ele não "deslizar" nas curvas
+    GetCharacterMovement()->GroundFriction = 8.0f;
 }
 
 void ARobotBase ::BeginPlay() {
--- Source/TempoRobotic/Public/Actors/RobotBase.h
+++ Source/TempoRobotic/Public/Actors/RobotBase.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "GameFramework/Character.h"
 #include "Structs/RobotTaskStruct.h"
 #include "Structs/RobotConfig.h"
 
 #include "RobotBase.generated.h"
 
 UCLASS()
-class TEMPOROBOTIC_API ARobotBase : public AActor {
+class TEMPOROBOTIC_API ARobotBase : public ACharacter {
 	GENERATED_BODY()
 	
 public:	
