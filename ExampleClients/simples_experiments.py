a3f4cb03c7c8d04493fc86fe0ff87c2ec9d426a4
renatoedesio <renatoedesio@unb.br>
2026-01-03 00:04:58 -0300

    Creating Move to Actor Task

--- ExampleClients/simples_experiments.py
+++ ExampleClients/simples_experiments.py
@@ -55,19 +55,27 @@ async def main():
     print("\n[Tarefa 1] Configurando: PrintToScreen Message...")
     await cmd_print_to_screen(robot_id, "THIS MESSAGE WILL APPEAR ON THE SCREEN FOR 10 SECONDS!!!", duration=10.0)
 
-      # Espera o feedback da Unreal
+    # Espera o feedback da Unreal
     sucesso = await wait_for_task_finished(robot_id, timeout=10)
     if not sucesso:
         print("A tarefa 1 falhou")
     """
     
+    """
     # --- TASK 2: Moving to point 0, 1500, 0
     print("\n[Tarefa 2] Configurando: MoveToLocation, vai andar 5 metros no eixo Y com aceitação de 10 unidades...")
     await cmd_move_to_location(robot_id, 1500, 500, 50.0)
     sucesso = await wait_for_task_finished(robot_id, timeout=50)
     if not sucesso:
         print("A tarefa 2 falhou")
+    """
 
+    # --- TASK 3: Moving to Actor
+    print("\n[Tarefa 2] Configurando: MoveToActor, vai procurar o Actor RoomAEntrance e se mover pra la...")
+    await cmd_move_to_actor(robot_id, "BP_RoomAEntrance")
+    sucesso = await wait_for_task_finished(robot_id, timeout=50)
+    if not sucesso:
+        print("A tarefa 2 falhou")
 
     print("\nFim do script.")
 
